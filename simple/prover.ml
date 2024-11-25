7d80cf80aa85aa3a9b8504d2eaa13ad67d9769c3
blueberry1342 <ycts2000@163.com>
2024-11-25 19:17:51 +0100

    Implemented the proof assistant.

--- simple/prover.ml
+++ simple/prover.ml
@@ -1,3 +1,9 @@
+open Expr
+
+let ty_of_string s = Parser.ty Lexer.token (Lexing.from_string s)
+
+let tm_of_string s = Parser.tm Lexer.token (Lexing.from_string s)
+
 let () = Printexc.record_backtrace true
 
 (** ocamlopt prover.ml -o prover
@@ -6,48 +12,24 @@ let () = Printexc.record_backtrace true
     git push
 *) 
 
-(** Type variables. *)
-type tvar = string
-
-(** Term variables. *)
-type var = string
-
-(** Types. *)
-type ty =
-  |Tvar of tvar
-  |Arr of ty*ty
-  |Cong of ty*ty
-  |Ttrue
-  |Disj of ty*ty
-  |Tfalse
-
-(** lambda-terms *)
-type tm =
-  |Var of var 
-  |App of tm*tm
-  |Abs of var*ty*tm
-  |Pair of tm*tm
-  |Fst of tm 
-  |Snd of tm
-  |True
-  |Left of tm*ty
-  |Right of tm*ty
-  |Match of tm*tm*tm
-  |Case of tm*ty
+(**
+dune exec ./prover.exe
+make
+*)
 
 
 (** string representation *)
 let rec string_of_ty (t:ty) : string = 
   match t with
-  |Tvar(v) -> v
-  |Arr(t',t'') -> "("^(string_of_ty t')^"⇒ "^(string_of_ty t'')^")"
-  |Cong(t',t'') -> "("^(string_of_ty t')^"∧"^(string_of_ty t'')^")"
-  |Ttrue -> "⊤"
-  |Disj(t',t'') -> "("^(string_of_ty t')^"∨"^(string_of_ty t'')^")"
-  |Tfalse -> "⊥ "
+  |TVar(v) -> v
+  |Imp(t',t'') -> "("^(string_of_ty t')^"⇒ "^(string_of_ty t'')^")"
+  |And(t',t'') -> "("^(string_of_ty t')^"∧"^(string_of_ty t'')^")"
+  |True -> "⊤"
+  |Or(t',t'') -> "("^(string_of_ty t')^"∨"^(string_of_ty t'')^")"
+  |False -> "⊥ "
 
 let () =
-  assert((string_of_ty (Arr(Arr(Tvar "A",Tvar "B"),Arr(Tvar "A",Tvar "C"))))="((A⇒ B)⇒ (A⇒ C))")
+  assert((string_of_ty (Imp(Imp(TVar "A",TVar "B"),Imp(TVar "A",TVar "C"))))="((A⇒ B)⇒ (A⇒ C))")
 
 let rec string_of_tm (t:tm) : string =
   match t with
@@ -57,13 +39,11 @@ let rec string_of_tm (t:tm) : string =
   |Pair(t',t'') -> "("^(string_of_tm t')^"∧"^(string_of_tm t'')^")"
   |Fst(t') -> "(Fst ("^(string_of_tm t')^"))"
   |Snd(t') -> "(Snd ("^(string_of_tm t')^"))"
-  |True -> "⊤"
-  |Left(tm,ty) -> "(Left ("^(string_of_tm tm)^"))"
-  |Right(tm,ty) -> "(Right ("^(string_of_tm tm)^"))"
+  |Unit -> "⊤"
   |_ -> "" (** to finish *)
 
 let () =
-  assert((string_of_tm (Abs("f",Arr(Tvar "A",Tvar "B"),Abs("x",Tvar "A",App(Var "f",Var "x")))))="(λ (f : (A⇒ B)) -> (λ (x : A) -> (f x)))")
+  assert((string_of_tm (Abs("f",Imp(TVar "A",TVar "B"),Abs("x",TVar "A",App(Var "f",Var "x")))))="(λ (f : (A⇒ B)) -> (λ (x : A) -> (f x)))")
 
 
 (** typing inference*)
@@ -83,10 +63,10 @@ let rec infer_type (c:context) (t:tm) : ty =
     )
     |App(t',t'') -> (
       match (infer_type c t') with
-        |Tvar v -> raise Type_error
-        |Arr(ty,ty') -> if ((infer_type c t'')=ty) then ty' else raise Type_error 
+        |TVar v -> raise Type_error
+        |Imp(ty,ty') -> if ((infer_type c t'')=ty) then ty' else raise Type_error 
     )
-    |Abs(v,ty,t') -> Arr(ty,infer_type c t')
+    |Abs(v,ty,t') -> Imp(ty,infer_type c t')
 
 (** type checking *)
 let check_type (c:context) (tm:tm) (ty:ty) : unit =
@@ -105,44 +85,38 @@ let rec infer_type (c:context) (t:tm) : ty =
     )
     |App(t',t'') -> (
       match (infer_type c t') with
-        |Arr(ty,ty') -> (
+        |Imp(ty,ty') -> (
           let () = check_type c t'' ty in ty'
         )
         |_ -> raise Type_error
     )
-    |Abs(v,ty,t') -> Arr(ty,infer_type ((v,ty)::c) t')
-    |Pair(t',t'') -> Cong(infer_type c t',infer_type c t'')
+    |Abs(v,ty,t') -> Imp(ty,infer_type ((v,ty)::c) t')
+    |Pair(t',t'') -> And(infer_type c t',infer_type c t'')
     |Fst(t') -> (
       match (infer_type c t') with
-        |Cong(ty,ty') -> ty
+        |And(ty,_) -> ty
         |_ -> raise Type_error
     )
     |Snd(t') -> (
       match (infer_type c t') with
-        |Cong(ty,ty') -> ty'
+        |And(_,ty') -> ty'
         |_ -> raise Type_error
     )
-    |True -> Ttrue
-    |Left(tm,ty) -> Disj(infer_type c tm,ty)
-    |Right(tm,ty) -> Disj(ty,infer_type c tm)
-    |Match(t',f1,f2) -> (
+    |Unit -> True
+    |Left(tm,ty) -> Or(infer_type c tm,ty)
+    |Right(ty,tm) -> Or(ty,infer_type c tm)
+    |Case(t',v1,f1,v2,f2) -> ( (***)
       match (infer_type c t') with
-        |Disj(a,b) -> (
-          match (infer_type c f1) with
-            |Arr(a',b') when a'=a -> (
-              match (infer_type c f2) with
-                |Arr(a'',b'') when a''=b -> (
-                  if (b'=b'') then b' else raise Type_error
-                )
-                |_ -> raise Type_error
-            )
-            |_ -> raise Type_error
+        |Or(a,b) -> (
+          let a' = (infer_type ((v1,a)::c) f1) in
+            if ((infer_type ((v2,b)::c) f2)=a') then a'
+            else raise Type_error
         )
         |_ -> raise Type_error
     )
-    |Case(tm,ty) -> (
+    |Absurd(tm,ty) -> (
       match (infer_type c tm) with
-        |Tfalse -> ty
+        |False -> ty
         |_ -> raise Type_error
     )
 and check_type (c:context) (tm:tm) (ty:ty) : unit =
@@ -152,13 +126,13 @@ and check_type (c:context) (tm:tm) (ty:ty) : unit =
 
 
 let () =
-  let x = Arr(Arr(Tvar "A",Tvar "B"),Arr(Arr(Tvar "B",Tvar "C"),Arr(Tvar "A", Tvar "C"))) in
-  let y = Abs("f",Arr(Tvar "A",Tvar "B"),Abs("g",Arr(Tvar "B",Tvar "C"),Abs("x",Tvar "A",App(Var "g",App(Var "f", Var "x"))))) in
+  let x = Imp(Imp(TVar "A",TVar "B"),Imp(Imp(TVar "B",TVar "C"),Imp(TVar "A", TVar "C"))) in
+  let y = Abs("f",Imp(TVar "A",TVar "B"),Abs("g",Imp(TVar "B",TVar "C"),Abs("x",TVar "A",App(Var "g",App(Var "f", Var "x"))))) in
   assert ((string_of_ty x)=(string_of_ty (infer_type [] y)))
 
 let () =
   try
-    let _ = (infer_type [] (Abs("f",Tvar "A", Var"x"))) in
+    let _ = (infer_type [] (Abs("f",TVar "A", Var"x"))) in
     assert false
   with
   | Type_error -> ()
@@ -166,7 +140,7 @@ let () =
 
 let () =
   try
-    let _ = (infer_type [] (Abs("f",Tvar "A", Abs("x",Tvar "B",App(Var "f",Var "x"))))) in
+    let _ = (infer_type [] (Abs("f",TVar "A", Abs("x",TVar "B",App(Var "f",Var "x"))))) in
     assert false
   with
   | Type_error -> ()
@@ -174,7 +148,7 @@ let () =
 
 let () =
   try
-    let _ = (infer_type [] (Abs("f",Arr(Tvar "A",Tvar "B"), Abs("x",Tvar "B",App(Var "f",Var "x"))))) in
+    let _ = (infer_type [] (Abs("f",Imp(TVar "A",TVar "B"), Abs("x",TVar "B",App(Var "f",Var "x"))))) in
     assert false
   with
   | Type_error -> ()
@@ -182,16 +156,16 @@ let () =
 
 
 let () =
-  try (check_type [] (Abs("x",Tvar "A",Var "x")) (Arr(Tvar "A",Tvar "A"))) with
+  try (check_type [] (Abs("x",TVar "A",Var "x")) (Imp(TVar "A",TVar "A"))) with
   |Type_error -> assert false
 
 let () =
-  try (check_type [] (Abs("x",Tvar "A",Var "x")) (Arr(Tvar "B",Tvar "B"))) with
+  try (check_type [] (Abs("x",TVar "A",Var "x")) (Imp(TVar "B",TVar "B"))) with
   |Type_error -> ()
   |_ -> assert false
 
 let () =
-  try (check_type [] (Var "x") (Tvar "A")) with
+  try (check_type [] (Var "x") (TVar "A")) with
   |Type_error -> ()
   |_ -> assert false
 
@@ -203,21 +177,21 @@ what to do ????????????????????????*)
 
 
 let () = 
-  let and_comm = Abs ("x",Cong(Tvar "A",Tvar "B"),Pair(Snd (Var "x"),Fst (Var "x"))) in
+  let and_comm = Abs ("x",And(TVar "A",TVar "B"),Pair(Snd (Var "x"),Fst (Var "x"))) in
   print_endline ("and_comm : "^(string_of_ty (infer_type [] and_comm)))
 
 (**  (⊤ ⇒ A) ⇒ A  *)
 let () =
-  let taa = Abs("f",Arr(Ttrue,Tvar "A"),App(Var "f",True)) in
+  let taa = Abs("f",Imp(True,TVar "A"),App(Var "f",Unit)) in
   print_endline ("(⊤ ⇒ A) ⇒ A : "^(string_of_ty (infer_type [] taa)))
 
-(** Disjunction-commutative *)
+(** Orunction-commutative *)
 let () =
-  let or_comm = Abs ("x",Disj(Tvar "A",Tvar "B"),Match(Var "x",Abs("y",Tvar "A",Right (Var "y",Tvar"B")),Abs("y",Tvar "B",Left (Var "y",Tvar"A")))) in
+  let or_comm = Abs ("x",Or(TVar "A",TVar "B"),Case(Var "x","y",Right (TVar"B",Var "y"),"y",Left (Var "y",TVar"A"))) in
   print_endline ("or_comm : "^(string_of_ty (infer_type [] or_comm)))
 
 
 (** (A∧(A⇒⊥))⇒B *)  
 let () =
-  let non_contr = Abs ("x",Cong(Tvar "A",Arr(Tvar "A",Tfalse)),Case(App(Snd (Var "x"),Fst (Var "x")),Tvar "B")) in
+  let non_contr = Abs ("x",And(TVar "A",Imp(TVar "A",False)),Absurd(App(Snd (Var "x"),Fst (Var "x")),TVar "B")) in
   print_endline ("non_contradiction : "^(string_of_ty (infer_type [] non_contr)))
