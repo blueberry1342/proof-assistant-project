a6b03b6c96b8f5a9ba820f6c10941a3f4da8afb5
blueberry1342 <ycts2000@163.com>
2024-11-26 13:30:46 +0100

    Implemented the proof assistant.

--- simple/prover.ml
+++ simple/prover.ml
@@ -17,8 +17,12 @@ dune exec ./prover.exe
 make
 *)
 
+(**
+cat NAME.proof | dune exec ./prover.exe
+*)
 
-(** string representation *)
+
+(** 1.3 string representation *)
 let rec string_of_ty (t:ty) : string = 
   match t with
   |TVar(v) -> v
@@ -37,22 +41,25 @@ let rec string_of_tm (t:tm) : string =
   |App(t',t'') -> "("^(string_of_tm t')^" "^(string_of_tm t'')^")"
   |Abs(v,ty,t') -> "(λ ("^v^" : "^(string_of_ty ty)^") -> "^(string_of_tm t')^")"
   |Pair(t',t'') -> "("^(string_of_tm t')^"∧"^(string_of_tm t'')^")"
-  |Fst(t') -> "(Fst ("^(string_of_tm t')^"))"
-  |Snd(t') -> "(Snd ("^(string_of_tm t')^"))"
-  |Unit -> "⊤"
-  |_ -> "" (** to finish *)
+  |Fst(t') -> "(fst("^(string_of_tm t')^"))"
+  |Snd(t') -> "(snd("^(string_of_tm t')^"))"
+  |Unit -> "()"
+  |Case(t,v1,f1,v2,f2) -> "case "^(string_of_tm t)^" of "^v1^" → "^(string_of_tm f1)^" | "^v2^" → "^(string_of_tm f2)
+  |Left(tm,ty) -> "left("^(string_of_tm tm)^","^(string_of_ty ty)^")"
+  |Right(ty,tm) -> "left("^(string_of_ty ty)^","^(string_of_tm tm)^")"
+  |Absurd(tm,ty) -> "absurd("^(string_of_tm tm)^","^(string_of_ty ty)^")"
 
 let () =
   assert((string_of_tm (Abs("f",Imp(TVar "A",TVar "B"),Abs("x",TVar "A",App(Var "f",Var "x")))))="(λ (f : (A⇒ B)) -> (λ (x : A) -> (f x)))")
 
 
-(** typing inference*)
+(** 1.4 typing inference*)
 type context = (var * ty) list
 
 exception Type_error
 
 
-(** seperated difinations of infer_type and type_checking *)
+(** 1.4 and 1.5 seperated difinations of infer_type and type_checking *)
 (*
 let rec infer_type (c:context) (t:tm) : ty =
   match t with
@@ -75,7 +82,7 @@ let check_type (c:context) (tm:tm) (ty:ty) : unit =
 *)
 
 
-(** type inference and checking together *)
+(** 1.6 type inference and checking together *)
 let rec infer_type (c:context) (t:tm) : ty =
   match t with
     |Var(v) -> (
@@ -175,23 +182,138 @@ let () =
 (*????????????????????????
 what to do ????????????????????????*)
 
-
+(** 1.8 and_comm *)
 let () = 
   let and_comm = Abs ("x",And(TVar "A",TVar "B"),Pair(Snd (Var "x"),Fst (Var "x"))) in
   print_endline ("and_comm : "^(string_of_ty (infer_type [] and_comm)))
 
-(**  (⊤ ⇒ A) ⇒ A  *)
+(** 1.9 (⊤ ⇒ A) ⇒ A  *)
 let () =
   let taa = Abs("f",Imp(True,TVar "A"),App(Var "f",Unit)) in
   print_endline ("(⊤ ⇒ A) ⇒ A : "^(string_of_ty (infer_type [] taa)))
 
-(** Orunction-commutative *)
+(** 1.10 Disjunction-commutative *)
 let () =
   let or_comm = Abs ("x",Or(TVar "A",TVar "B"),Case(Var "x","y",Right (TVar"B",Var "y"),"y",Left (Var "y",TVar"A"))) in
   print_endline ("or_comm : "^(string_of_ty (infer_type [] or_comm)))
 
 
-(** (A∧(A⇒⊥))⇒B *)  
+(** 1.11 (A∧(A⇒⊥))⇒B *)  
 let () =
   let non_contr = Abs ("x",And(TVar "A",Imp(TVar "A",False)),Absurd(App(Snd (Var "x"),Fst (Var "x")),TVar "B")) in
   print_endline ("non_contradiction : "^(string_of_ty (infer_type [] non_contr)))
+
+
+ (** 1.12 Parsing strings *) 
+let () =
+  let l = [
+    "A => B";
+    "A ⇒ B";
+    "A /\\ B";
+    "A ∧ B";
+    "T";
+    "A \\/ B";
+    "A ∨ B";
+    "_";
+    "not A";
+    "¬ A";
+  ]
+  in
+  List.iter
+    (fun s ->
+       Printf.printf
+         "the parsing of %S is %s\n%!" s (string_of_ty (ty_of_string s))
+    ) l
+
+let () =
+  let l = [
+    "t u v";
+    "fun (x : A) -> t";
+    "λ (x : A) → t";
+    "(t , u)";
+    "fst(t)";
+    "snd(t)";
+    "()";
+    "case t of x -> u | y -> v";
+    "left(t,B)";
+    "right(A,t)";
+    "absurd(t,A)"
+  ]
+  in
+  List.iter
+    (fun s ->
+       Printf.printf
+         "the parsing of %S is %s\n%!" s (string_of_tm (tm_of_string s))
+    ) l
+
+
+(** 2.1 String representation of contexts *)
+let rec string_of_ctx (c:context) : string = 
+  match c with
+    |[] -> ""
+    |(var,ty)::c' when c'<>[] -> (string_of_ctx c')^" , "^var^" : "^(string_of_ty ty)
+    |(var,ty)::_ -> var^" : "^(string_of_ty ty)
+
+(** 2.2 Sequents *)
+type sequent = context * ty
+
+let string_of_seq (s:sequent) : string =
+  match s with
+    |(c,ty) -> (string_of_ctx c)^" |- "^(string_of_ty ty)
+
+(**  2.3 An interactive prover *)
+let rec prove env a =
+  print_endline (string_of_seq (env,a));
+  print_string "? "; flush_all ();
+  let error e = print_endline e; prove env a in
+  let cmd, arg =
+    let cmd = input_line stdin in
+    let n = try String.index cmd ' ' with Not_found -> String.length cmd in
+    let c = String.sub cmd 0 n in
+    let a = String.sub cmd n (String.length cmd - n) in
+    let a = String.trim a in
+    c, a
+  in
+  match cmd with
+  | "intro" ->
+     (
+       match a with
+       | Imp (a, b) ->
+          if arg = "" then error "Please provide an argument for intro." else
+            let x = arg in
+            let t = prove ((x,a)::env) b in
+            Abs (x, a, t)
+       | _ ->
+          error "Don't know how to introduce this."
+     )
+  | "exact" ->
+     let t = tm_of_string arg in
+     if infer_type env t <> a then error "Not the right type."
+     else t
+  |"elim" ->(
+    match a with
+     |TVar(b) -> (
+        if arg = "" then error "Please provide an argument for intro." else
+        let x = arg in
+        let ty = infer_type env (Var x) in
+        match ty with
+          |Imp(a',b') when b'=(TVar b) -> let t = prove env a' in App(Var x,t)
+          |_ -> error "Not the right type."
+      )
+      |_ ->
+        error "Don't know how to eliminate this."
+  )
+  | cmd -> error ("Unknown command: " ^ cmd)
+         
+let () =
+  print_endline "Please enter the formula to prove:";
+  let a = input_line stdin in
+  let a = ty_of_string a in
+  print_endline "Let's prove it.";
+  let t = prove [] a in
+  print_endline "done.";
+  print_endline "Proof term is";
+  print_endline (string_of_tm t);
+  print_string  "Typechecking... "; flush_all ();
+  assert (infer_type [] t = a);
+  print_endline "ok."
